b8466bcf9bc1f424be77c5c08c6830ecaf5f200c
Michael Sanford <mikethecodegeek@gmail.com>
2016-07-25 15:27:19 -0700

    cors

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api', require('./routes/api'));
 app.use('/', require('./routes/index'));
